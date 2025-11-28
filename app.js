c84e183fb95cc47728a93a49d4d31b912449b009
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-28 16:49:01 +0100

    style: create structure for hoempage and styling

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ require("dotenv").config({path: "./.env.local"})
 
 const app = express();
 app.set("view engine", 'ejs')
+app.use(express.static("public"))
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Working")
+    res.render("index")
 })
 
 app.listen(PORT, console.log(`Server is listening on: ${PORT}`))
