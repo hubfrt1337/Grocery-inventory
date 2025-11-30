61ac1a45ce3a270a9863c30a1bfec7173a16bfeb
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-30 20:53:14 +0100

    feat: add event listener to the form to show and hide it

--- public/product.js
+++ public/product.js
@@ -1,6 +1,18 @@
- /*const btn = document.querySelector(".add-js")
+const btn = document.querySelector(".add-js")
+const formContainer = document.querySelector(".form-container")
+const formAdd = document.querySelector(".form-add")
 
 btn.addEventListener("click", () => {
-    console.log("małpa")
+    formContainer.style.display = "flex"
 })
-    */
+
+formAdd.addEventListener('submit', () => {
+    formContainer.style.display = 'none';
+})
+
+window.addEventListener("click", (e) => {
+    if(e.target === formContainer){
+        formContainer.style.display = 'none';
+    }
+    
+})
